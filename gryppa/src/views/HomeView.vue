37f692f23041c3fd40859492be5cff5d106fdebd
Maksim Tsymbal <maksimcymbal784@email.com>
2024-11-27 15:14:37 +0500

    add layout main page

--- gryppa/src/views/HomeView.vue
+++ gryppa/src/views/HomeView.vue
@@ -3,7 +3,73 @@
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="contener">
+    <div class="row md-4">
+      <div class="col-md-5">
+       <select class="form-select" aria-label="Default select example">
+  <option selected>Select category</option>
+  <option value="1">VideoCard</option>
+  <option value="2">CPU</option>
+  <option value="3">MotherBoard</option>
+  <option value="4">RAM</option>
+</select>
+</div>
+
+      <div class="col-md-5">
+        <input type="email" class="form-control"  placeholder="Search by name...">
+      </div>
+      <div class="col-md-2">
+        <button type="button" class="btn btn-info">Reset</button>
+      </div>
+    </div>
+    <div class="card-group">
+      <div class="col">
+  <div class="card">
+    <img src="https://static.onlinetrade.ru/img/items/b/materinskaya_plata_asrock_z270_gaming_k4_lga1151_atx__2.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Материнская плата ASRock Z270 GAMING K4 (LGA1151, ATX)</h5>
+      <p class="card-text">$400</p>
+      <p class="card-text">MotherBoard</p>
+    </div>
+ 
+    <div class="card-footer text-end">
+    <button type="button" class="btn btn-success m-2">Datele</button>
+    <button type="button" class="btn btn-secondary">card</button>
+  </div>
+ </div>
+  </div>
+  <div class="col">
+  <div class="card">
+    <img src="https://avatars.mds.yandex.net/i?id=eafc6329a5f82b7e78730bf9f3cdc824_l-5234613-images-thumbs&n=13" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Процессор AMD Ryzen 5 7500F, 3.7ГГц,</h5>
+      <p class="card-text">$1500</p>
+      <p class="card-text"><small class="text-body-secondary">CPU</small></p>
+    </div>
+  
+    <div class="card-footer text-end">
+    <button type="button" class="btn btn-success m-2">Datele</button>
+    <button type="button" class="btn btn-secondary">card</button>
+  </div>
+</div>
+  <div class="col">
+  <div class="card">
+    <img src="https://avatars.mds.yandex.net/i?id=a860486763713160891e4322feae3b5d_l-5869639-images-thumbs&n=13" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Samsung Now Mass Producing Industry’s Most Advanced DDR4, Using 20 Nanometer-class Process Technology</h5>
+      <p class="card-text">$1000</p>
+      <p class="card-text"><small class="text-body-secondary">RAM</small></p>
+    </div>
+  
+    <div class="card-footer text-end">
+    <button type="button" class="btn btn-success m-2">Datele</button>
+    <button type="button" class="btn btn-secondary">card</button>
+  </div>
+</div>
+</div>
+    </div>
+  </div>
+  </div>
+
+  
 </template>
